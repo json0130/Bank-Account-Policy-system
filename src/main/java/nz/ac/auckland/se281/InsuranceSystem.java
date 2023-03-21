edc6b1449086ce4d6f98bf24ef2351d028ae1012
Jay <json941@aucklanduni.ac.nz>
2023-03-21 21:09:12 +1300

    Can correctly print out the user as well as the print_db and can detect whether the username is long enough or not

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,31 +1,22 @@
 package nz.ac.auckland.se281;
 
 import java.util.ArrayList;
-
-import javax.swing.plaf.TreeUI;
-
-import org.eclipse.jgit.lib.UserConfig;
-import org.eclipse.jgit.transport.CredentialItem.Username;
-
-import com.fasterxml.jackson.core.sym.Name;
-
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
-    
+
   }
+
   public void printDatabase() {
 
-    if(data_list.size() < 1){
+    if (data_list.size() < 1) {
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
-    }
-    else if(data_list.size() == 1){
+    } else if (data_list.size() == 1) {
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", "", ":");
       printProfile();
-    }
-    else{
+    } else {
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage(Integer.toString(data_list.size()), "s", ":");
       printProfile();
     }
@@ -33,25 +24,26 @@ public class InsuranceSystem {
 
   private ArrayList<Person> data_list = new ArrayList<>();
 
-  public void createNewProfile(String Name,String age) {
-    
-    String fixedName = Name.substring(0,1).toUpperCase() + Name.substring(1).toLowerCase();
+  public void createNewProfile(String Name, String age) {
+
+    String fixedName = Name.substring(0, 1).toUpperCase() + Name.substring(1).toLowerCase();
     Person user1 = new Person(fixedName, age);
-    
-    if(checkuser(fixedName) == true){
-      if(checkage(fixedName,age) == true){
-        data_list.add(user1);
-        MessageCli.PROFILE_CREATED.printMessage(fixedName,age);
+
+    if (checkuser(fixedName) == true) {
+      if (fixedName.length() < 3) {
+        MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(fixedName);
+      } else {
+        if (checkage(fixedName, age) == true) {
+          data_list.add(user1);
+          MessageCli.PROFILE_CREATED.printMessage(fixedName, age);
+        } else {
+          data_list.remove(user1);
+          MessageCli.INVALID_AGE.printMessage(age, fixedName);
+        }
       }
-      else{
-        data_list.remove(user1);
-        MessageCli.INVALID_AGE.printMessage(age,fixedName);
-      }
-    }
-    else{
+    } else {
       MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(fixedName);
     }
-    
   }
 
   public void loadProfile(String userName) {
@@ -70,32 +62,32 @@ public class InsuranceSystem {
     // TODO: Complete this method.
   }
 
-  public void printProfile(){
-    for(int i = 0;i < data_list.size();i++) {   
-      //MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(Integer.toString(i+1),);//
-    
+  public void printProfile() {
+    for (int i = 0; i < data_list.size(); i++) {
+      // MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(Integer.toString(i+1),);//
+
       Person Name = data_list.get(i);
       System.out.print(" " + Integer.toString(i + 1) + ": ");
       Name.printDetails();
     }
   }
 
-  public boolean checkuser(String fixedName){
-    for (Person username : data_list){
-        if(fixedName.equals(username.getName())){
-          return false;
-        }
+  public boolean checkuser(String fixedName) {
+    for (Person username : data_list) {
+      if (fixedName.equals(username.getName())) {
+        return false;
       }
+    }
     return true;
   }
 
-  public boolean checkage(String fixedName,String age){
+  public boolean checkage(String fixedName, String age) {
     try {
-        if(Integer.parseInt(age) > 0){
-          return true;
-        }else{
-          return false;
-        }  
+      if (Integer.parseInt(age) > 0) {
+        return true;
+      } else {
+        return false;
+      }
     } catch (Exception e) {
       return false;
     }
